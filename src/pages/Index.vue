cd1a67e0de50fe5940f1e98779c2b51df6a2caca
Jose Duarte <joseduarte019@gmail.com>
2021-02-10 17:40:04 -0400

    add transitions

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -38,9 +38,9 @@
             <q-img src="responsive.png" style='height: auto; max-width: 70%'/>
           </div>
 
-          <q-intersection>
+          <q-intersection id="about">
             <transition appear enter-active-class="animated fadeInRight delay-1s" >
-          <div id="about" class="bg-grey-2 row col-12">
+          <div  class="bg-grey-2 row col-12">
             <p class="q-mt-lg q-mb-lg row col-12 justify-center text-h2">About</p>
             <div class="row col-12" >
               <q-card class="row col-xs-12 col-sm-4 col-md-4 col-lg-4 q-pa-md">
@@ -292,119 +292,7 @@ export default {
       mongoBar: 0.6,
       slide: 1,
     };
-  },
-  mounted() {
-    particlesJS("particles-js", {
-      particles: {
-        number: {
-          value: 80,
-          density: {
-            enable: true,
-            value_area: 800,
-          },
-        },
-        color: {
-          value: "#ffffff",
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#000000",
-          },
-          polygon: {
-            nb_sides: 5,
-          },
-          image: {
-            src: "img/github.svg",
-            width: 100,
-            height: 100,
-          },
-        },
-        opacity: {
-          value: 0.5,
-          random: false,
-          anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false,
-          },
-        },
-        size: {
-          value: 3,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
-            sync: false,
-          },
-        },
-        line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#ffffff",
-          opacity: 0.4,
-          width: 1,
-        },
-        move: {
-          enable: true,
-          speed: 6,
-          direction: "none",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200,
-          },
-        },
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: true,
-            mode: "grab",
-          },
-          onclick: {
-            enable: true,
-            mode: "push",
-          },
-          resize: true,
-        },
-        modes: {
-          grab: {
-            distance: 140,
-            line_linked: {
-              opacity: 1,
-            },
-          },
-          bubble: {
-            distance: 400,
-            size: 40,
-            duration: 2,
-            opacity: 8,
-            speed: 3,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-          push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
-          },
-        },
-      },
-      retina_detect: true,
-    });
-  },
+  },    
   methods: {
     openLink(link) {
       switch (link) {
